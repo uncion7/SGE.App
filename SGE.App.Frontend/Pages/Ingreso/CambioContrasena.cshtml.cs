72c463a2752034886ac0940d2da6a660046880ea
uncion7 <uncion7@gmail.com>
2021-10-17 16:27:55 -0500

    sincronizado

--- SGE.App.Frontend/Pages/Ingreso/CambioContrasena.cshtml.cs
+++ SGE.App.Frontend/Pages/Ingreso/CambioContrasena.cshtml.cs
@@ -2,33 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Http;
-using SGE.App.Persistencia;
-using SGE.App.Dominio;
 
 namespace SGE.App.Frontend.Pages
 {
-    public class IngresoModel : PageModel
+    public class CambioContrasenaModel : PageModel
     {
-        
         private SGE.App.Persistencia.AppContext _appContext;
         
 
         [BindProperty]
         public string Usuario {get;set;}
+
+        [BindProperty]
+        public string Mail {get;set;}
         
         [BindProperty]
-        public string Password{get;set;}
+        public string NewPassword{get;set;}
 
         [BindProperty]
         public string MensajeUsuario{get;set;}
         
         [BindProperty]
-        public string MensajePassword{get;set;}
+        public string MensajeCorreo{get;set;}
+
+        [BindProperty]
+        public string MensajeExito{get;set;}
 
         public void OnGet()
         {
@@ -36,32 +36,30 @@ namespace SGE.App.Frontend.Pages
 
         public IActionResult OnPost()
         {
-            _appContext = new SGE.App.Persistencia.AppContext();
+             _appContext = new SGE.App.Persistencia.AppContext();
 
             var p = _appContext.Usuarios.FirstOrDefault( p => p.User == Usuario);
             if(p == null)
             {
-                MensajeUsuario = "Usuario no existe";
+                MensajeUsuario = "Usuario no existe.";
                 //Console.WriteLine("User error");
             }
             
-            else if (!p.Contrasena.Equals(Password))
+            else if (!p.Correo.Equals(Mail))
             {
                 //Console.WriteLine("Contraseña incorrecta");
-                MensajePassword = "Contraseña incorrecta";
+                MensajeCorreo = "Correo incorrecto.";
             }
-            else if(p.Entro == false)
-            {
-                return RedirectToPage("./CambioContrasena");
-            }
-
             else
             {
-            return RedirectToPage("../Index");
+                p.Entro = true;
+                p.Contrasena = NewPassword;
+                _appContext.SaveChanges();
+                MensajeExito = "Su contrasena fue cambiada con exito.";
+                return RedirectToPage("../Index");
             }
 
             return Page();
-
         }
 
     }
