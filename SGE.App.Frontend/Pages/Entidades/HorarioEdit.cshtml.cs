5ee89a6122338e9c0864bf83be9a2151190fec6e
uncion7 <uncion7@gmail.com>
2021-10-17 16:26:52 -0500

    Validaciones Formador y Horarios

--- SGE.App.Frontend/Pages/Entidades/HorarioEdit.cshtml.cs
+++ SGE.App.Frontend/Pages/Entidades/HorarioEdit.cshtml.cs
@@ -26,12 +26,19 @@ namespace SGE.App.Frontend.Pages
         [BindProperty]
         public Horario Horario {get;set;}
 
+        public Usuario Formador {get; set;}
+        
         [BindProperty]
-        public string MsgHorarioLimite {get; set;}
+        public string MsgIntervalo {get; set;}
 
         [BindProperty]
-        public string MsgGrupoLimite {get; set;}
+        public string MsgIntensidad {get; set;}
 
+        [BindProperty]
+        public string MsgDia {get; set;}
+
+        [BindProperty]
+        public string MsgFormador {get; set;}
 
         public HorarioEditModel(IRepositorioHorario repositorioHorario, SGE.App.Persistencia.AppContext appContext)
         {
@@ -39,27 +46,96 @@ namespace SGE.App.Frontend.Pages
             _appContext = appContext;
         }
 
-        public bool validarHorario(TimeSpan hInicial, TimeSpan hFinal)
+        public bool validarIntervalo(Horario Horario)
         {
+            bool intervaloValido;
             //Validar Horario
             TimeSpan t1 = new TimeSpan(2, 0, 0);
             TimeSpan t2 = new TimeSpan(3, 0, 0);
-            TimeSpan h1 = hFinal - hInicial;
+            TimeSpan h1 = Horario.HoraFinal - Horario.HoraInicial;
 
             if(h1 >= t1 && h1 <= t2)
             {
-                return true;
+                intervaloValido = true;
             }
             else
             {
-                return false;
+                intervaloValido = false;
             }
+            return intervaloValido;
         }
 
-        //Validar Grupo: 7 horas semanales de Lunes a Sábado
-        public bool validarGrupo(Grupo Grupo)
+        //Validar Grupo: 7 horas semanales de Lunes a Sábado.
+        public bool validarIntensidad(Horario Horario)
         {
-            return true;
+            //Intensidad Horaria: 7 horas semanales
+            bool intensidadValida;
+            var intensidadSemanalQuery = _appContext.Horarios.Where(p => p.Grupo == Horario.Grupo);
+            TimeSpan intensidadLimite = new TimeSpan(7,0,0);
+            TimeSpan intensidadSemanal = new TimeSpan(0,0,0);
+            TimeSpan intensidadNueva = Horario.HoraFinal - Horario.HoraInicial;
+            foreach (var item in intensidadSemanalQuery)
+            {
+                var intensidadDia = item.HoraFinal - item.HoraInicial;
+                intensidadSemanal = intensidadSemanal + intensidadDia; 
+            }
+            var intensidadTotal = intensidadSemanal + intensidadNueva;
+            
+            //Validacion intensidad horaria
+            if(intensidadTotal <= intensidadLimite)
+            {
+                intensidadValida = true;
+            }
+            else
+            {
+                intensidadValida = false;
+            }
+            return intensidadValida;
+        }
+
+        //El Grupo no se repite el mismo día
+        public bool validarDia(Horario Horario)
+        {
+            //Grupo no repetido el mismo día
+            bool diaValido;
+            var diaNoExisteQuery = _appContext.Horarios
+                .Where(p => p.Grupo == Horario.Grupo)
+                .Where(p => p.DiaSemana == Horario.DiaSemana)
+                .Count();
+            
+            if(diaNoExisteQuery == 0)
+            {
+                diaValido = true;
+            }
+            else
+            {
+                diaValido = false;
+            }
+            return diaValido;
+        }
+
+        public bool validarHorarioFormador(Horario Horario, Usuario Formador)
+        {
+            bool hfValido;
+
+            var hfQuery = _appContext.Horarios
+                .Include(p => p.Grupo)
+                .ThenInclude(p => p.Formador)
+                .Where(p => p.Grupo.Formador == Formador)
+                .Where(p => p.DiaSemana == Horario.DiaSemana)
+                .Where(p => p.HoraInicial == Horario.HoraInicial)
+                .Where(p => p.HoraFinal == Horario.HoraFinal)
+                .Count();
+            
+            if(hfQuery == 0)
+            {
+                hfValido = true;
+            }
+            else
+            {
+                hfValido = false;
+            }
+            return hfValido;
         }
 
         public IActionResult OnGet(int? horarioId)
@@ -96,7 +172,9 @@ namespace SGE.App.Frontend.Pages
 
             var listaGruposDb = _appContext.Grupos;
             listaGrupos = new SelectList(listaGruposDb, nameof(Grupo.Id), nameof(Grupo.Nombre));
-            Grupo grupo = _appContext.Grupos.FirstOrDefault(g => g.Id == GrupoID);
+            Grupo grupo = _appContext.Grupos
+                .Include(g => g.Formador)
+                .FirstOrDefault(g => g.Id == GrupoID);
             Horario.Grupo = grupo;
             Horario.DiaSemana = DiaID;
 
@@ -107,30 +185,41 @@ namespace SGE.App.Frontend.Pages
             else
             {   
                 //Validar Horario y Grupo
-                var hValido = validarHorario(Horario.HoraInicial, Horario.HoraFinal);
-                var gValido = validarGrupo(Horario.Grupo);
+                var intervaloOk = validarIntervalo(Horario);
+                var intensidadOk = validarIntensidad(Horario);
+                var diaOk = validarDia(Horario);
+                var formadorOk = validarHorarioFormador(Horario, grupo.Formador);
+                string mensajeIntervalo = "El horario debe ser entre 2 y 3 horas";
+                string mensajeIntensidad = "El Grupo sobrepasa la intensidad horaria de 7 semanales de Lunes a Sabado";
+                string mensajeDia = "El Grupo ya tiene asignado este dia de la semana"; 
+                string mensajeFormador = "El Formador ya tiene este horario ocupado";
 
-                if(hValido == true && gValido == true)
+                if(intervaloOk == true && intensidadOk == true && diaOk == true && formadorOk == true)
                 {
                     Horario = repositorioHorario.AddHorario(Horario);    
                 }
-                else if(hValido == false && gValido == false)
+                else
                 {
-                    MsgHorarioLimite = "El horario debe ser entre 2 y 3 horas";
-                    MsgGrupoLimite = "El Grupo ya tiene 7 horas semanales de Lunes a Sábado";
+                    if(intervaloOk == false)
+                    {
+                        MsgIntervalo = mensajeIntervalo;
+                    }
+
+                    if(intensidadOk == false)
+                    {
+                        MsgIntensidad = mensajeIntensidad;
+                    }
+                    
+                    if(diaOk == false)
+                    {
+                        MsgDia = mensajeDia;
+                    }
+                    if(formadorOk == false)
+                    {
+                        MsgFormador = mensajeFormador;
+                    }
                     return Page();
                 }
-                else if(hValido == false && gValido == true)
-                {
-                    MsgHorarioLimite = "El horario debe ser entre 2 y 3 horas";
-                    return Page();
-                }
-                else if(hValido == true && gValido == false)
-                {
-                    MsgGrupoLimite = "El Grupo ya tiene 7 horas semanales de Lunes a Sábado";
-                    return Page();
-                }
-                
             }
             return RedirectToPage("./HorarioList");
         }
